f3ea74cc71daf0fe90c92f2fb6f3912df58b1725
James Moey <jamesmoey@gmail.com>
2011-01-08 12:09:42 +1100

    2 new method to do encoding and decoding between Windows Encoding and UTF-8

--- components/TextHelper.php
+++ components/TextHelper.php
@@ -1,7 +1,6 @@
 <?php
 class TextHelper 
 {
-
   public static $randomTextBoundary = "QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm0123456789";
 
   public static function generateRandomText($length = 8) {
@@ -10,5 +9,32 @@ class TextHelper
       $string .= self::$randomTextBoundary[rand(0,strlen(self::$randomTextBoundary)-1)];
     }
     return $string;
+  }
+
+  /**
+   * Convert Windows Encoding to UTF-8
+   * @static
+   * @param string|array $input
+   */
+  public static function encodeWindowEncodingToUtf8(&$input) {
+    if (is_array($input)) {
+      foreach ($input as $key=>&$value) {
+        $input[$key] = self::encodeWindowEncodingToUtf8($value);
+      }
+    } else {
+      $input = @iconv('windows-1256', 'UTF-8', $input);
+    }
+    return $input;
+  }
+
+  public static function decodeUtf8ToWindowEncoding(&$input) {
+    if (is_array($input)) {
+      foreach ($input as $key=>&$value) {
+        $input[$key] = self::decodeUtf8ToWindowEncoding($value);
+      }
+    } else {
+      $input = @iconv('UTF-8', 'windows-1256', $input);
+    }
+    return $input;
   }
 }
