3cbbfbee5440c176749513e9b73157e112c0eeed
James Moey <jamesmoey@gmail.com>
2011-02-01 00:29:44 +1100

    Add new method to allow this behavior to be use with Data Access Object.

--- behaviors/FormatConverterBehavior.php
+++ behaviors/FormatConverterBehavior.php
@@ -42,12 +42,73 @@ class FormatConverterBehavior extends CActiveRecordBehavior {
     return $columns;
   }
 
-  public function beforeSave($event) {
-    /** @var CActiveRecord $model */
+  public function afterFind($event) {
+    $this->afterConstruct($event);
+  }
+
+  public function afterConstruct($event) {
     $model = $this->getOwner();
+    $attributes = $model->getAttributes();
+    $this->convert(array($attributes), array($this, 'setModelValue'));
+  }
+
+  public function setModelValue($field, $value) {
+    $model = $this->getOwner();
+    $model->setAttribute($field, $value);
+  }
+
+  public function convert($attributesRef, $function = NULL) {
+    $attributes =& $attributesRef[0];
+    foreach ($this->_dateVariables as $field) {
+      $time = strtotime($attributes[$field]);
+      if ($time <= 0) {
+        if ($function != NULL) call_user_func($function, $field, '');
+        else $attributes[$field] = '';
+        continue;
+      }
+      if (!empty($this->dateFormat)) {
+        $date = Yii::app()->getDateFormatter()->format($this->dateFormat, $time);
+      }
+      if (strlen($date) == 0 && Yii::app()->hasComponent("formatter")) {
+        /** @var CFormatter $formatter */
+        $formatter = Yii::app()->getComponent('formatter');
+        $date = $formatter->formatDate($time);
+      }
+      if (strlen($date) == 0) $date = date('d-m-Y', $time);
+      if (strlen($date) != 0) {
+        Yii::trace("Converting ".$attributes[$field].' to '.$date);
+        if ($function != NULL) call_user_func($function, $field, $date);
+        else $attributes[$field] = $date;
+      }
+    }
+    foreach ($this->_datetimeVariables as $field) {
+      $time = strtotime($attributes[$field]);
+      if ($time <= 0) {
+        call_user_func($function, $field, '');
+        continue;
+      }
+      if (!empty($this->dateTimeFormat)) {
+        $date = Yii::app()->getDateFormatter()->format($this->dateTimeFormat, $time);
+      }
+      if (strlen($date) == 0 && Yii::app()->hasComponent("formatter")) {
+        /** @var CFormatter $formatter */
+        $formatter = Yii::app()->getComponent('formatter');
+        $date = $formatter->formatDatetime($time);
+      }
+      if (strlen($date) == 0) $date = date('d-m-Y H:i:s', $time);
+      if (strlen($date) != 0) {
+        Yii::trace("Converting ".$attributes[$field].' to '.$date);
+        if ($function != NULL) call_user_func($function, $field, $date);
+        else $attributes[$field] = $date;
+      }
+    }
+  }
+
+  public  function revert($attributesRef, $function = NULL) {
+    $attributes =& $attributesRef[0];
     foreach ($this->_dateVariables as $field) {
       $time = 0;
-      $date = $model->getAttribute($field);
+      $date = $attributes[$field];
       if (!empty($this->dateFormat)) $time = CDateTimeParser::parse($date, $this->dateFormat);
       if ($time == 0 && Yii::app()->hasComponent("formatter")) {
         /** @var CFormatter $formatter */
@@ -56,11 +117,14 @@ class FormatConverterBehavior extends CActiveRecordBehavior {
       }
       if ($time == 0) $time = CDateTimeParser::parse($date, Yii::app()->getLocale()->getDateFormat());
       if ($time == 0) $time = strtotime($date);
-      if ($time > 0) $model->setAttribute($field, date('Y-m-d', $time));
+      if ($time > 0) {
+        if ($function != NULL) call_user_func($function, $field, date('Y-m-d', $time));
+        else $attributes[$field] = date('Y-m-d', $time);
+      }
     }
     foreach ($this->_datetimeVariables as $field) {
       $time = 0;
-      $date = $model->getAttribute($field);
+      $date = $attributes[$field];
       if (!empty($this->dateTimeFormat)) $time = CDateTimeParser::parse($date, $this->dateTimeFormat);
       if ($time == 0 && Yii::app()->hasComponent("formatter")) {
         /** @var CFormatter $formatter */
@@ -69,8 +133,18 @@ class FormatConverterBehavior extends CActiveRecordBehavior {
       }
       if ($time == 0) $time = CDateTimeParser::parse($date, Yii::app()->getLocale()->getDateTimeFormat());
       if ($time == 0) $time = strtotime($date);
-      if ($time > 0) $model->setAttribute($field, date('Y-m-d H:i:s', $time));
+      if ($time > 0) {
+        if ($function != NULL) call_user_func($function, $field, date('Y-m-d H:i:s', $time));
+        else $attributes[$field] = date('Y-m-d', $time);
+      }
     }
+  }
+
+  public function beforeSave($event) {
+    /** @var CActiveRecord $model */
+    $model = $this->getOwner();
+    $attributes = $model->getAttributes();
+    $this->revert(array($attributes), array($this, 'setModelValue'));
     return true;
   }
 
